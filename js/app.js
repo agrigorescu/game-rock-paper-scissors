1962d2366150094fe935a863d5b841efc14b5877
andreea grigorescu <deea26@gmail.com>
2016-11-26 12:56:54 +0000

    rearranged the code structure in the js file. user choice works, computer choice works, it goes into the function however it falls on the default in the switch everytime. need to reconsider the switch

--- js/app.js
+++ js/app.js
@@ -6,21 +6,9 @@ $(function(){
       console.log("button clicked");
     });
 
-    //
-    var userChoice;
-    var userChoose = $('.myButtons');
-    for (var i = 0; i < userChoose.length; i++) {
-        var button = userChoose[i];
-        button.addEventListener('click' , function(event) {
-            console.log(this.id + ' was clicked'); // this always refers to the button that was clicked
-            userChoice = this.id; //assigning the user chioce to a variable
-            console.log(userChoice);
-        });
-    }
-
-
     //assigning a random number to the computer choice variable
     var computerChoice = Math.random();
+    console.log("computer choice is: "+computerChoice);
 
     //checking the computer's choice.
     //the rule is 'rock' between 0 and 0.34, 'paper' between 0.34 and 0.67, 'sciccors' between 0.67 and 1
@@ -34,42 +22,58 @@ $(function(){
             computerChoice = "scissors";
             console.log("computerChoice is "+computerChoice);
         }
+
+    //setting up the user choice to be read from the buttons event listener
+    var userChoice;
+    var userChoose = $('.myButtons');
+    for (var i = 0; i < userChoose.length; i++) {
+        var button = userChoose[i];
+        button.addEventListener('click' , function(event) {
+            console.log(this.id + ' was clicked'); // this always refers to the button that was clicked
+            userChoice = this.id.substring(0, this.id.length); //assigning the user chioce to a variable
+            console.log("this is user choice: "+userChoice);
+             //calling the compareChoice function
+            compareChoice(computerChoice,userChoice);
+        });
+    }
     
     //creating a function to check the userChoice and the computerChoice
-    function compareChoice(choice1,choice2){
+    function compareChoice(choiceComputer,choiceUser){
+        let choice1 = String(choiceComputer);
+        let choice2 = String(choiceUser);
+        console.log(choice1+" "+choice2);
         switch (choice1,choice2){
-            case "choice1===choice2" : 
+            case choice1===choice2 : 
                     console.log("it's a tie");
                     break;
-            case "choice1==='rock' && choice2==='scissors'":
+            case choice1==='rock' && choice2==='scissors':
                     console.log("rock wins. computer wins");
                     $("#ccount").html(parseInt($("#ccount").html())+1);
                     break;
-            case "choice1==='rock' && choice2==='paper'":
+            case choice1==='rock' && choice2==='paper':
                     console.log("paper wins. user wins");
                     $("#count").html(parseInt($("#count").html())+1);
                     break;
-            case "choice1==='paper' && choice2==='scissors'":
+            case choice1==='paper' && choice2==='scissors':
                     console.log("scissors wins. user wins");
                     $("#count").html(parseInt($("#count").html())+1);
                     break;
-            case "choice1==='paper' && choice2==='rock'":
+            case choice1==='paper' && choice2==='rock':
                     console.log("paper wins. computer wins");
                     $("#ccount").html(parseInt($("#ccount").html())+1);
                     break;
-            case "choice1==='scissors' && choice2==='rock'":
+            case choice1==='scissors' && choice2==='rock':
                     console.log("rock wins.user wins");
                     $("#count").html(parseInt($("#count").html())+1);
                     break;
-            case "choice1==='scissors' && choice2==='paper'":
+            case choice1==='scissors' && choice2==='paper':
                     console.log("scissors wins.computer wins");
                     $("#ccount").html(parseInt($("#ccount").html())+1);
                     break;
+            default:
+                    console.log("something went wrong");
         } 
     } 
-
-    //calling the compareChoice function
-    compareChoice(computerChoice,userChoice);
 });
 
 
