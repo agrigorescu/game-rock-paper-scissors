c5418eda3d2ac071ffcbc1d483f99418b65faf2c
andreea grigorescu <deea26@gmail.com>
2016-11-26 12:25:13 +0000

    wrote the compareChoice function. it does not work. needs research.

--- js/app.js
+++ js/app.js
@@ -26,78 +26,50 @@ $(function(){
     //the rule is 'rock' between 0 and 0.34, 'paper' between 0.34 and 0.67, 'sciccors' between 0.67 and 1
      if ( computerChoice < 0.34) {
          computerChoice = "rock";
-         console.log(computerChoice);
+         console.log("computerChoice is "+computerChoice);
      } else if (computerChoice < 0.67){
          computerChoice = "paper";
-         console.log(computerChoice);
+         console.log("computerChoice is "+computerChoice);
         } else {
             computerChoice = "scissors";
-            console.log(computerChoice);
+            console.log("computerChoice is "+computerChoice);
         }
     
     //creating a function to check the userChoice and the computerChoice
     function compareChoice(choice1,choice2){
         switch (choice1,choice2){
-            case "choice1===choice2" : console.log(it's a tie');
-
-        }
-        
-
+            case "choice1===choice2" : 
+                    console.log("it's a tie");
+                    break;
+            case "choice1==='rock' && choice2==='scissors'":
+                    console.log("rock wins. computer wins");
+                    $("#ccount").html(parseInt($("#ccount").html())+1);
+                    break;
+            case "choice1==='rock' && choice2==='paper'":
+                    console.log("paper wins. user wins");
+                    $("#count").html(parseInt($("#count").html())+1);
+                    break;
+            case "choice1==='paper' && choice2==='scissors'":
+                    console.log("scissors wins. user wins");
+                    $("#count").html(parseInt($("#count").html())+1);
+                    break;
+            case "choice1==='paper' && choice2==='rock'":
+                    console.log("paper wins. computer wins");
+                    $("#ccount").html(parseInt($("#ccount").html())+1);
+                    break;
+            case "choice1==='scissors' && choice2==='rock'":
+                    console.log("rock wins.user wins");
+                    $("#count").html(parseInt($("#count").html())+1);
+                    break;
+            case "choice1==='scissors' && choice2==='paper'":
+                    console.log("scissors wins.computer wins");
+                    $("#ccount").html(parseInt($("#ccount").html())+1);
+                    break;
+        } 
     } 
 
-
-
+    //calling the compareChoice function
+    compareChoice(computerChoice,userChoice);
 });
 
 
-// var compare = function (choice1,choice2)
-// {
-//     if (choice1===choice2)
-//     {
-//         return "The result is a tie!";
-//     }
-//     else
-//     {
-//         if(choice1==="rock")
-//         {
-//             if(choice2==="scissors")
-//             {
-//                 return "rock wins";
-//             }
-//             else
-//             {
-//                 return "paper wins";
-//             }
-//         }
-//          else
-//         {
-//             if(choice1==="paper")
-//             {
-//                 if(choice2==="rock")
-//                 {
-//                     return "paper wins";
-//                 }
-//                 else
-//                 {
-//                     return "scissors wins";
-//                 }
-//             }
-//             else
-//             {
-//                 if(choice1==="scissors")
-//                 {
-//                     if(choice2==="rock")
-//                     {
-//                         return "rock wins";
-//                     }
-//                     else
-//                     {
-//                         return "scissors wins";
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// compare(userChoice,computerChoice);
